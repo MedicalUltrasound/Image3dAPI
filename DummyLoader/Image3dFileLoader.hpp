86875075541ad93a97443d3a9f7fe909bc991385
Fredrik Orderud <fredrik.orderud@ge.com>
2015-11-14 14:38:47 +0100

    Add skeleton impl of Image3dFileLoader.

---
+++ DummyLoader/Image3dFileLoader.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "../Image3dAPI/ComSupport.hpp"
+#include "../Image3dAPI/IImage3d.h"
+
+
+[coclass,
+default(IImage3dSource),                ///< default interface
+threading(both),                        ///< "both" enables direct thread access without marshaling
+vi_progid("DummyLoader.Image3dFileLoader"), ///< version-independent name
+progid("DummyLoader.Image3dFileLoader.1"),  ///< class name
+version(1.0),
+uuid(8E754A72-0067-462B-9267-E84AF84828F1), ///< class ID (must be unique)
+helpstring("3D image file loader")]
+class Image3dFileLoader : public IImage3dFileLoader {
+public:
+    Image3dFileLoader() {
+    }
+
+    /*NOT virtual*/ ~Image3dFileLoader() {
+    }
+
+    HRESULT STDMETHODCALLTYPE LoadFile(BSTR file_name, /*[out]*/ BSTR *error_message) {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE GetImageSource(/*[out]*/ IImage3dSource **img_src) {
+        return E_NOTIMPL;
+    }
+};
--- DummyLoader/Main.cpp
+++ DummyLoader/Main.cpp
@@ -1,4 +1,4 @@
 #include "Image3dSource.hpp"
-
+#include "Image3dFileLoader.hpp"
 
 [module(dll, name = "DummyLoader", version = "1.0", uuid = "67E59584-3F6A-4852-8051-103A4583CA5E", helpstring = "DummyLoader module")];
