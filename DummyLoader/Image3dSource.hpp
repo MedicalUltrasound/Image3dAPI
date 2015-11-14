5a913132edfe5fdc190c1612ce0d563c5313dfa7
Fredrik Orderud <fredrik.orderud@ge.com>
2015-11-14 14:44:36 +0100

    Impl. Image3dSource::GetFrameCount().

--- DummyLoader/Image3dSource.hpp
+++ DummyLoader/Image3dSource.hpp
@@ -14,41 +14,48 @@ uuid(6FA82ED5-6332-4344-8417-DEA55E72098C), ///< class ID (must be unique)
 helpstring("3D image source")]
 class Image3dSource : public IImage3dSource {
 public:
-    Image3dSource() {
+    Image3dSource () : m_frame_count(1) {
     }
 
-    /*NOT virtual*/ ~Image3dSource() {
+    /*NOT virtual*/ ~Image3dSource () {
     }
 
-    HRESULT STDMETHODCALLTYPE GetFrameCount(/*[out]*/ unsigned int *size) {
+    HRESULT STDMETHODCALLTYPE GetFrameCount (/*[out]*/ unsigned int *size) {
+        if (!size)
+            return E_INVALIDARG;
+
+        *size = m_frame_count;
+        return S_OK;
+    }
+
+    HRESULT STDMETHODCALLTYPE GetFrameTimes (/*[out]*/ SAFEARRAY * *frame_times) {
         return E_NOTIMPL;
     }
 
-    HRESULT STDMETHODCALLTYPE GetFrameTimes(/*[out]*/ SAFEARRAY * *frame_times) {
+    HRESULT STDMETHODCALLTYPE GetFrame (unsigned int index, Cart3dGeom geom, /*[out]*/ Image3d *data) {
         return E_NOTIMPL;
     }
 
-    HRESULT STDMETHODCALLTYPE GetFrame(unsigned int index, Cart3dGeom geom, /*[out]*/ Image3d *data) {
+    HRESULT STDMETHODCALLTYPE GetBoundingBox (/*[out]*/ Cart3dGeom *geom) {
         return E_NOTIMPL;
     }
 
-    HRESULT STDMETHODCALLTYPE GetBoundingBox(/*[out]*/ Cart3dGeom *geom) {
+    HRESULT STDMETHODCALLTYPE GetColorMap (/*[out]*/ unsigned int __MIDL__IImage3dSource0000[256]) {
         return E_NOTIMPL;
     }
 
-    HRESULT STDMETHODCALLTYPE GetColorMap(/*[out]*/ unsigned int __MIDL__IImage3dSource0000[256]) {
+    HRESULT STDMETHODCALLTYPE GetECG (/*[out]*/ EcgSeries *ecg) {
         return E_NOTIMPL;
     }
 
-    HRESULT STDMETHODCALLTYPE GetECG(/*[out]*/ EcgSeries *ecg) {
+    HRESULT STDMETHODCALLTYPE GetProbeInfo (/*[out]*/ ProbeInfo *probe) {
         return E_NOTIMPL;
     }
 
-    HRESULT STDMETHODCALLTYPE GetProbeInfo(/*[out]*/ ProbeInfo *probe) {
+    HRESULT STDMETHODCALLTYPE GetSopInstanceUID (/*[out] */ BSTR *uid_str) {
         return E_NOTIMPL;
     }
 
-    HRESULT STDMETHODCALLTYPE GetSopInstanceUID(/*[out] */ BSTR *uid_str) {
-        return E_NOTIMPL;
-    }
+private:
+    unsigned int m_frame_count;
 };
