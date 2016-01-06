64d97e58cea09b444fcc7a831241ee0cc8cbc387
Fredrik Orderud <fredrik.orderud@ge.com>
2016-01-06 22:46:07 +0100

    Update repo URL.

--- PackagingGE/BuildNuGet.bat
+++ PackagingGE/BuildNuGet.bat
@@ -29,7 +29,7 @@ git log %PREV_TAG%..%NEW_TAG% --decorate --graph --stat > changelog.txt
 
 
 echo Update NuGet package version and project URL:
-python.exe ..\PackagingGE\SetAutopkgVersion.py %AUTOPKG_FILE% %NEW_TAG% https://github.build.ge.com/CardiovascularUltrasound/Image3dAPI/tree/%NEW_TAG%
+python.exe ..\PackagingGE\SetAutopkgVersion.py %AUTOPKG_FILE% %NEW_TAG% https://github.com/MedicalUltrasound/Image3dAPI/tree/%NEW_TAG%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 
