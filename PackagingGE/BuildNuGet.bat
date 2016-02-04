62558fd74ab834f7b478c32f07e187e793bb4951
Joger Hansegard <joger.hansegard@ge.com>
2016-02-04 20:59:07 +0100

    Build from the root level to allow introducing secondary modules

--- PackagingGE/BuildNuGet.bat
+++ PackagingGE/BuildNuGet.bat
@@ -5,10 +5,10 @@ set PATH=%PATH%;C:\Python27
 set PATH=%PATH%;"C:\Program Files\Git\bin"
 set AUTOPKG_FILE=..\PackagingGE\Image3dAPI.autopkg
 
-cd ..\Image3dAPI
+pushd ..
 
 echo Building project:
-msbuild /nologo /verbosity:minimal /target:Build /property:Configuration="Debug";Platform="x64" Image3dAPI.vcxproj
+msbuild /nologo /verbosity:minimal /target:Build /property:Configuration="Debug";Platform="x64" Image3dApi\Image3dAPI.vcxproj
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 echo Determine previous tag:
@@ -16,7 +16,7 @@ git describe --abbrev=0 --tags > PREV_TAG.txt
 set /p PREV_TAG=< PREV_TAG.txt
 
 echo Creating new GIT tag:
-python.exe ..\PackagingGE\DetermineNextTag.py minor > NEW_TAG.txt
+python.exe PackagingGE\DetermineNextTag.py minor > NEW_TAG.txt
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 set /p NEW_TAG=< NEW_TAG.txt
 git tag %NEW_TAG%
@@ -27,7 +27,10 @@ if DEFINED NUGET_REPO (
 echo Generating changelog (with tag decoration, graph and change stats):
 git log %PREV_TAG%..%NEW_TAG% --decorate --graph --stat > changelog.txt
 
-CALL ../PackagingGE/PackagePublishNuget.bat %AUTOPKG_FILE% %NEW_TAG% %NUGET_REPO%
+pushd Image3dApi
+CALL ..\PackagingGE\PackagePublishNuget.bat %AUTOPKG_FILE% %NEW_TAG% %NUGET_REPO%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
+popd
 
+popd
 echo [done]
