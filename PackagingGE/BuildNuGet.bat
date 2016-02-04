3819b5ffb88159f1862ebac945633e686a5f2eac
Joger Hansegard <joger.hansegard@ge.com>
2016-02-04 21:09:44 +0100

    Add build and packaging of DummyLoader

--- PackagingGE/BuildNuGet.bat
+++ PackagingGE/BuildNuGet.bat
@@ -1,14 +1,24 @@
 @echo off
 set PRIMARY_NUGET_REPO=%1
+set SECONDARY_NUGET_REPO=%2
 
 set PATH=%PATH%;C:\Python27
 set PATH=%PATH%;"C:\Program Files\Git\bin"
 set AUTOPKG_FILE=..\PackagingGE\Image3dAPI.autopkg
 
+
 pushd ..
 
-echo Building project:
+echo Building projects:
 msbuild /nologo /verbosity:minimal /target:Build /property:Configuration="Debug";Platform="x64" Image3dApi\Image3dAPI.vcxproj
+IF %ERRORLEVEL% NEQ 0 exit /B 1
+msbuild /nologo /verbosity:minimal /target:Build /property:Configuration="Debug";Platform="x64" DummyLoader\DummyLoader.vcxproj
+IF %ERRORLEVEL% NEQ 0 exit /B 1
+msbuild /nologo /verbosity:minimal /target:Build /property:Configuration="Release";Platform="x64" DummyLoader\DummyLoader.vcxproj
+IF %ERRORLEVEL% NEQ 0 exit /B 1
+msbuild /nologo /verbosity:minimal /target:Build /property:Configuration="Debug";Platform="Win32" DummyLoader\DummyLoader.vcxproj
+IF %ERRORLEVEL% NEQ 0 exit /B 1
+msbuild /nologo /verbosity:minimal /target:Build /property:Configuration="Release";Platform="Win32" DummyLoader\DummyLoader.vcxproj
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 echo Determine previous tag:
@@ -29,6 +39,11 @@ git log %PREV_TAG%..%NEW_TAG% --decorate --graph --stat > changelog.txt
 
 pushd Image3dApi
 CALL ..\PackagingGE\PackagePublishNuget.bat %AUTOPKG_FILE% %NEW_TAG% %PRIMARY_NUGET_REPO%
+IF %ERRORLEVEL% NEQ 0 exit /B 1
+popd
+
+pushd DummyLoader
+CALL ..\PackagingGE\PackagePublishNuget.bat ..\PackagingGE\DummyLoader.autopkg %NEW_TAG% %PRIMARY_NUGET_REPO%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 popd
 
