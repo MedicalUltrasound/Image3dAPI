68f34226952625d2c5820e5e2497303789147f65
Fredrik Orderud <fredrik.orderud@ge.com>
2017-08-07 10:31:21 +0200

    Change main to be ATL-based. This is boilerplate code taken from the Visual Studio ATL project wizard.

--- DummyLoader/Main.cpp
+++ DummyLoader/Main.cpp
@@ -1,14 +1,74 @@
-/* Dummy test loader for the "3D API".
-Designed by Fredrik Orderud <fredrik.orderud@ge.com>.
-Copyright (c) 2016, GE Healthcare, Ultrasound.      */
-
 #include "Image3dSource.hpp"
 #include "Image3dFileLoader.hpp"
 
-[module(
-#ifdef _WINDLL
-    dll,
-#else
-    exe,
-#endif
-    name = "DummyLoader", version = "1.0", uuid = "67E59584-3F6A-4852-8051-103A4583CA5E", helpstring = "DummyLoader module")];
+#include "resource.h"
+#include "DummyLoader.h"
+#include "DummyLoader_i.c"
+
+
+class DummyLoaderModule :
+    public ATL::CAtlDllModuleT<DummyLoaderModule>
+{
+public:
+    DECLARE_LIBID(LIBID_DummyLoader)
+    DECLARE_REGISTRY_APPID_RESOURCEID(IDR_DUMMYLOADER, "{0BE0C352-0B9F-432E-9CE9-8EC32CC9EF68}")
+};
+
+DummyLoaderModule _AtlModule;
+
+
+
+// DLL Entry Point
+extern "C" BOOL WINAPI DllMain(HINSTANCE /*hInstance*/, DWORD dwReason, LPVOID lpReserved)
+{
+    return _AtlModule.DllMain(dwReason, lpReserved);
+}
+
+// Used to determine whether the DLL can be unloaded by OLE.
+STDAPI DllCanUnloadNow()
+{
+    return _AtlModule.DllCanUnloadNow();
+}
+
+// Returns a class factory to create an object of the requested type.
+_Check_return_
+STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID* ppv)
+{
+    return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
+}
+
+// DllRegisterServer - Adds entries to the system registry.
+STDAPI DllRegisterServer()
+{
+    // registers object, typelib and all interfaces in typelib
+    return _AtlModule.DllRegisterServer();
+}
+
+// DllUnregisterServer - Removes entries from the system registry.
+STDAPI DllUnregisterServer()
+{
+    return _AtlModule.DllUnregisterServer();
+}
+
+// DllInstall - Adds/Removes entries to the system registry per user per machine.
+STDAPI DllInstall(BOOL bInstall, _In_opt_  LPCWSTR pszCmdLine)
+{
+    static const wchar_t szUserSwitch[] = L"user";
+
+    if (pszCmdLine != NULL) {
+        if (_wcsnicmp(pszCmdLine, szUserSwitch, _countof(szUserSwitch)) == 0)
+            ATL::AtlSetPerUserRegistration(true);
+    }
+
+    HRESULT hr = E_FAIL;
+    if (bInstall) {
+        hr = DllRegisterServer();
+        if (FAILED(hr))
+            DllUnregisterServer();
+    }
+    else {
+        hr = DllUnregisterServer();
+    }
+
+    return hr;
+}
