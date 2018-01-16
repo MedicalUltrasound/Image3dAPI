7c5c159caa7d277f65ed9426e980f025405e9ff6
Fredrik Orderud <fredrik.orderud@ge.com>
2018-01-16 23:12:07 +0100

    Implement preliminary image display
    Show three fixed orthogonal slices through the center of the image volume.
    
    NOTICE: Color-map handling is not yet implemented.

--- TestViewer/MainWindow.xaml.cs
+++ TestViewer/MainWindow.xaml.cs
@@ -65,6 +65,89 @@ namespace TestViewer
 
         private void DrawImages (uint frame)
         {
+            Debug.Assert(m_source != null);
+
+            // retrieve image volume
+            Cart3dGeom bbox  = m_source.GetBoundingBox();
+            ushort[] max_res = new ushort[]{ 128, 128, 128 };
+            Image3d image = m_source.GetFrame(frame, bbox, max_res);
+
+            FrameTime.Text = "Frame time: " + image.time;
+
+            {
+                // extract center-X slize
+                WriteableBitmap bitmap = new WriteableBitmap(image.dims[1], image.dims[2], 96.0, 96.0, PixelFormats.Bgr24, null);
+                bitmap.Lock();
+                unsafe {
+                    int x = image.dims[0] / 2;
+                    for (int y = 0; y < bitmap.Height; ++y)
+                    {
+                        for (int z = 0; z < bitmap.Width; ++z)
+                        {
+                            byte val = image.data[x + y * image.stride0 + z * image.stride1];
+
+                            byte* pixel = (byte*)bitmap.BackBuffer + z * (bitmap.Format.BitsPerPixel / 8) + y * bitmap.BackBufferStride;
+                            pixel[0] = val; // blue
+                            pixel[1] = val; // green
+                            pixel[2] = val; // red
+                        }
+                    }
+                }
+                bitmap.AddDirtyRect(new Int32Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight));
+                bitmap.Unlock();
+
+                Image1.Source = bitmap;
+            }
+            {
+                // extract center-Y slize
+                WriteableBitmap bitmap = new WriteableBitmap(image.dims[0], image.dims[2], 96.0, 96.0, PixelFormats.Bgr24, null);
+                bitmap.Lock();
+                unsafe
+                {
+                    int y = image.dims[1] / 2;
+                    for (int z = 0; z < bitmap.Height; ++z)
+                    {
+                        for (int x = 0; x < bitmap.Width; ++x)
+                        {
+                            byte val = image.data[x + y * image.stride0 + z * image.stride1];
+
+                            byte* pixel = (byte*)bitmap.BackBuffer + x * (bitmap.Format.BitsPerPixel / 8) + z * bitmap.BackBufferStride;
+                            pixel[0] = val; // blue
+                            pixel[1] = val; // green
+                            pixel[2] = val; // red
+                        }
+                    }
+                }
+                bitmap.AddDirtyRect(new Int32Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight));
+                bitmap.Unlock();
+
+                Image2.Source = bitmap;
+            }
+            {
+                // extract center-Z slize
+                WriteableBitmap bitmap = new WriteableBitmap(image.dims[0], image.dims[1], 96.0, 96.0, PixelFormats.Bgr24, null);
+                bitmap.Lock();
+                unsafe
+                {
+                    int z = image.dims[2] / 2;
+                    for (int y = 0; y < bitmap.Height; ++y)
+                    {
+                        for (int x = 0; x < bitmap.Width; ++x)
+                        {
+                            byte val = image.data[x + y * image.stride0 + z * image.stride1];
+
+                            byte* pixel = (byte*)bitmap.BackBuffer + x * (bitmap.Format.BitsPerPixel / 8) + y * bitmap.BackBufferStride;
+                            pixel[0] = val; // blue
+                            pixel[1] = val; // green
+                            pixel[2] = val; // red
+                        }
+                    }
+                }
+                bitmap.AddDirtyRect(new Int32Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight));
+                bitmap.Unlock();
+
+                Image3.Source = bitmap;
+            }
         }
     }
 }
