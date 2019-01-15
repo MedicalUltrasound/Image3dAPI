858815ae01a07e39f00a13017d2c2019c124a5cb
Fredrik Orderud <fredrik.orderud@ge.com>
2019-01-15 13:26:44 +0100

    Remove support for old "autopkg" format for NuGet packaging, that is no longer in use.

--- PackagingGE/SetAutopkgVersion.py
+++ PackagingGE/SetAutopkgVersion.py
@@ -20,9 +20,8 @@ def IdentifyDependencies (filename):
     return dep_dict
 
 
-def UpdateAutopkg (filename, version, url, dependencies):
-    '''Set the minor version in an autopkg or nuspec file'''
-    IS_NUSPEC = os.path.splitext(filename)[-1].lower() == '.nuspec'
+def UpdateNuspec (filename, version, url, dependencies):
+    '''Set the minor version in an nuspec file'''
     
     for line in fileinput.input(filename, inplace=True):
         if '<<GIT-VERSION>>' in line:
@@ -31,41 +30,27 @@ def UpdateAutopkg (filename, version, url, dependencies):
         elif '<<SOURCE-URL>>' in line:
             print(line.replace('<<SOURCE-URL>>', url), end='')
         elif '<<VERSION>>' in line:
-            if IS_NUSPEC:
-                # line on "<dependency id="xxx" version="<<VERSION>>" />" format
-                tokens = line.split()
-                for token in tokens:
-                    if token[:4] == 'id="':
-                        package = token[4:-1]
-                    elif token[:6] == 'version="':
-                        pass # assume that tag contains "<<VERSION>>" string
-                line = '        <dependency id="'+package+'" version="'+dependencies[package]+'" />'
-                print(line, end='\n')
-            else:
-                # use version from nuget config
-                idx = line.find('/<<VERSION>>')
-                package = line[:idx].split()[-1]
-                print(line.replace('<<VERSION>>', dependencies[package]), end='')
+            # line on "<dependency id="xxx" version="<<VERSION>>" />" format
+            tokens = line.split()
+            for token in tokens:
+                if token[:4] == 'id="':
+                    package = token[4:-1]
+                elif token[:6] == 'version="':
+                    pass # assume that tag contains "<<VERSION>>" string
+            line = '        <dependency id="'+package+'" version="'+dependencies[package]+'" />'
+            print(line, end='\n')
         elif '<<DEPENDENCIES>>' in line:
-            if IS_NUSPEC:
-                # convert dependency dict to nuspec XML representation
-                dep_str = ''
-                for key in dependencies:
-                    dep_str += '<dependency id="'+key+'" version="'+dependencies[key]+'" />\n        '
-                print(line.replace('<<DEPENDENCIES>>', dep_str), end='')
-            else:
-                # convert dependency dict to string representation on 'dep-name/version'-format
-                dep_str = ''
-                for key in dependencies:
-                    dep_str += key+'/'+dependencies[key]+', '
-                dep_str = dep_str[:-2] # rm trailing ', '
-                print(line.replace('<<DEPENDENCIES>>', dep_str), end='')
+            # convert dependency dict to nuspec XML representation
+            dep_str = ''
+            for key in dependencies:
+                dep_str += '<dependency id="'+key+'" version="'+dependencies[key]+'" />\n        '
+            print(line.replace('<<DEPENDENCIES>>', dep_str), end='')
         else:
             print(line, end='')
 
 
 if __name__ == "__main__":
-    filename = sys.argv[1] # autopkg file
+    filename = sys.argv[1] # nuspec file
     version  = sys.argv[2] # version number
     url      = sys.argv[3] # release URL
     
@@ -82,4 +67,4 @@ if __name__ == "__main__":
                 deps[key] = tmp[key]
     
     print('Setting version and URL of '+filename+' to '+version+' and '+url)
-    UpdateAutopkg(filename, version, url, deps)
+    UpdateNuspec(filename, version, url, deps)
