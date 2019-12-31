0946548391f78fc531e1548f4f7c34317f52fef5
Fredrik Orderud <fredrik.orderud@ge.com>
2019-12-31 14:47:37 +0100

    Update DummyLoader GUID's to prepare for a new API release
    Done to enable old (v1.2) and new (v2.0) DummyLoader to co-exist on the same system within interfering with one another.

--- DummyLoader/UNREGISTER_DummyLoader.bat
+++ DummyLoader/UNREGISTER_DummyLoader.bat
@@ -10,18 +10,18 @@ cd /d "%~dp0"
 :: Remove all traces of DummyLoader from registry
 for %%R in (HKEY_LOCAL_MACHINE HKEY_CURRENT_USER) do (
   :: TypeLib
-  reg delete "%%R\SOFTWARE\Classes\TypeLib\{67E59584-3F6A-4852-8051-103A4583CA5E}" /f 2> NUL
+  reg delete "%%R\SOFTWARE\Classes\TypeLib\{ED4540BD-07B5-44B0-BCDE-3E2C1D99183B}" /f 2> NUL
 
   for %%P in (32 64) do (
     :: Image3dSource class
     reg delete "%%R\SOFTWARE\Classes\DummyLoader.Image3dSource"                    /f 2> NUL
     reg delete "%%R\SOFTWARE\Classes\DummyLoader.Image3dSource.1"                  /f 2> NUL
-    reg delete "%%R\SOFTWARE\Classes\CLSID\{6FA82ED5-6332-4344-8417-DEA55E72098C}" /f /reg:%%P 2> NUL
+    reg delete "%%R\SOFTWARE\Classes\CLSID\{50BE330D-F729-4D8F-A1E4-C939E0598EDF}" /f /reg:%%P 2> NUL
 
     :: Image3dFileLoader class
     reg delete "%%R\SOFTWARE\Classes\DummyLoader.Image3dFileLoader"                /f 2> NUL
     reg delete "%%R\SOFTWARE\Classes\DummyLoader.Image3dFileLoader.1"              /f 2> NUL
-    reg delete "%%R\SOFTWARE\Classes\CLSID\{8E754A72-0067-462B-9267-E84AF84828F1}" /f /reg:%%P 2> NUL
+    reg delete "%%R\SOFTWARE\Classes\CLSID\{1326A2C6-7753-4584-B866-CDF3C6E240F1}" /f /reg:%%P 2> NUL
   )
 )
 
