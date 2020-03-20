26dda0a66d90bc9e89b9d6bb0f1079a200854685
Ingmar Voigt <ingmar.voigt@siemens-healthineers.com>
2020-03-20 21:30:31 +0100

    TestPython: Refactor ITKExport.py and TestPython.py into separate projects

--- TestPython/ITKExport.py
+++ TestPython/ITKExport.py
@@ -3,8 +3,35 @@ import platform
 import comtypes
 import comtypes.client
 import numpy as np
+import SimpleITK as sitk
 from utils import SafeArrayToNumpy
 from utils import FrameTo3dArray
+
+def SaveITKImage(imgFrame, bbox, outputFilename):
+    array = FrameTo3dArray(imgFrame)
+    itk_img = sitk.GetImageFromArray(array.astype("float64"))
+
+    m2mm = 1000
+
+    dir1   = [bbox.dir1_x,   bbox.dir1_y,   bbox.dir1_z]
+    dir2   = [bbox.dir2_x,   bbox.dir2_y,   bbox.dir2_z]
+    dir3   = [bbox.dir3_x,   bbox.dir3_y,   bbox.dir3_z]
+
+    # all units from Image3dAPI are in meters according to https://github.com/MedicalUltrasound/Image3dAPI/wiki#image-geometry
+    spacingX = np.linalg.norm(dir1) / imgFrame.dims[0] * m2mm # convert from meters to millimeters
+    spacingY = np.linalg.norm(dir2) / imgFrame.dims[1] * m2mm
+    spacingZ = np.linalg.norm(dir3) / imgFrame.dims[2] * m2mm
+
+
+    dir1 = dir1 / np.linalg.norm(dir1)
+    dir2 = dir2 / np.linalg.norm(dir2)
+    dir3 = dir3 / np.linalg.norm(dir3)
+
+    itk_img.SetSpacing((spacingX,spacingY,spacingZ))
+    itk_img.SetOrigin((bbox.origin_x*m2mm, bbox.origin_y*m2mm, bbox.origin_z*m2mm))
+    itk_img.SetDirection((dir1[0], dir2[0], dir3[0], dir1[1], dir2[1], dir3[1], dir1[2], dir2[2], dir3[2]))
+
+    sitk.WriteImage(itk_img, outputFilename)
 
 if __name__=="__main__":
     # load type library
@@ -21,18 +48,6 @@ if __name__=="__main__":
     err_type, err_msg = loader.LoadFile("dummy.dcm")
     source = loader.GetImageSource()
 
-    # retrieve probe info
-    probe = source.GetProbeInfo()
-    print("Probe name: "+probe.name)
-    print("Probe type: "+str(probe.type))
-
-    # retrieve ECG info
-    ecg = source.GetECG()
-    print("ECG start: "+str(ecg.start_time))
-    print("ECG delta: "+str(ecg.delta_time))
-    samples = SafeArrayToNumpy(ecg.samples)
-    trig_times = SafeArrayToNumpy(ecg.trig_times)
-
     # get bounding box
     bbox = source.GetBoundingBox()
     origin = [bbox.origin_x, bbox.origin_y, bbox.origin_z]
@@ -47,12 +62,5 @@ if __name__=="__main__":
     for i in range(frame_count):
         max_res = np.ctypeslib.as_ctypes(np.array([64, 64, 64], dtype=np.ushort))
         frame = source.GetFrame(i, bbox, max_res)
-        
-        print("Frame metadata:")
-        print("  time:   "+str(frame.time))
-        print("  format: "+str(frame.format))
-        print("  dims:  ("+str(frame.dims[0])+", "+str(frame.dims[1])+", "+str(frame.dims[2])+")")
-
         data = FrameTo3dArray(frame)
-        print("  shape: "+str(data.shape))
-
+        SaveITKImage(frame, bbox, "image3DAPIDummOutput" + str(i) + ".mhd")
